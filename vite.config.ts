afc3c7aabd28b663e9658b10eced1affe12a7bf9
Koh Uehara <refluster@gmail.com>
2026-04-04 13:53:36 -0700

    basename refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { SITE_BASE_PATH } from './src/config/site'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ai-native-article',
+  base: SITE_BASE_PATH,
 })
