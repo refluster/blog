f2f99aae60e8f885c13499b8c345f463dfdadea5
Koh Uehara <refluster@gmail.com>
2026-04-04 20:31:50 -0700

    webapp url for cors

--- src/lib/gas-config.ts
+++ src/lib/gas-config.ts
@@ -7,5 +7,6 @@
  * Format: https://script.google.com/macros/s/{DEPLOYMENT_ID}/usercontent
  */
 
-export const GAS_URL = 'https://script.google.com/macros/s/AKfycbwXqYjaYhtXOjlKo5AIO6iARRhIm5oOQ6sQsesqD5o/usercontent'
+//export const GAS_URL = 'https://script.google.com/macros/s/AKfycbwXqYjaYhtXOjlKo5AIO6iARRhIm5oOQ6sQsesqD5o/usercontent'
+export const GAS_URL = 'https://script.google.com/macros/s/AKfycbyxnsRuZsuMKBTwDDu6KxVQQ8g5BKAE_vZHH9VFMp2ZL3ub8az_DggcX9VJQ9_93pD1/usercontent'
 
